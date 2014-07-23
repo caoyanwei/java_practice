48731e046855708446f7e8d72654bcf10f6551c2
freshsky2012 <fresksky2012@gmail.com>
2014-07-23 22:18:55 +0800

    modify ProxyTest

--- utils/src/test/java/com/kwk/reflect/ProxyTest.java
+++ utils/src/test/java/com/kwk/reflect/ProxyTest.java
@@ -2,10 +2,7 @@ package com.kwk.reflect;
 
 import org.junit.Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -19,9 +16,27 @@ public class ProxyTest {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 System.out.println("method:" + method.getName());
+
+                Annotation[][] annotations = method.getParameterAnnotations();
+                for (int j = 0; j < annotations.length; j++) {
+                    int length = annotations[j].length;
+                    if (length == 0) {
+                        System.out.println("没有添加Annotation参数:" + j);
+                    } else {
+                        for (int k = 0; k < length; k++) {
+                            Annotation annotation = annotations[j][k];
+                            if (annotation instanceof ArgName) {
+                                ArgName argName = (ArgName)annotation;
+                                System.out.println("argName.name:" + argName.name());
+                            }
+                        }
+                    }
+                }
+
                 for (Object obj : args) {
                     System.out.println("para class type:" + obj.getClass().getName());
                 }
+
                 return null;
             }
         });
@@ -39,12 +54,18 @@ public class ProxyTest {
 
 interface MyItf {
     @MyAnnotation(source = "mysource")
-    String test(int a, int b);
+    String test(int a, @ArgName(name = "bbb") int b);
 }
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @interface MyAnnotation {
     String source();
+}
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@interface ArgName {
+    String name();
 }
 
