b594877dab98599c31afcb504aa8dd5a636bb19d
yanwei.cao <yanwei.cao@dianping.com>
2015-05-22 10:35:07 +0800

    modify NotifyMsgTest

--- utils/src/test/java/com/kwk/jackson/NotifyMsgTest.java
+++ utils/src/test/java/com/kwk/jackson/NotifyMsgTest.java
@@ -1,11 +1,18 @@
 package com.kwk.jackson;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
 public class NotifyMsgTest {
 
     @Test
-    public void basicTest() {
+    public void basicTest() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        NotifyMsg notifyMsg = new NotifyMsg();
+        notifyMsg.setBriefInfo("aaaaa");
+        notifyMsg.setDetailUrl("http://aaa.com/abc?clubId=1&dpUserId=${dpUserId}");
+        System.out.println(mapper.writeValueAsString(notifyMsg));
 
     }
 }
@@ -13,4 +20,20 @@ public class NotifyMsgTest {
 class NotifyMsg {
     private String briefInfo;
     private String detailUrl;
+
+    public String getBriefInfo() {
+        return briefInfo;
+    }
+
+    public void setBriefInfo(String briefInfo) {
+        this.briefInfo = briefInfo;
+    }
+
+    public String getDetailUrl() {
+        return detailUrl;
+    }
+
+    public void setDetailUrl(String detailUrl) {
+        this.detailUrl = detailUrl;
+    }
 }
