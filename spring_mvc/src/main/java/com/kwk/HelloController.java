425240ee0a4c6fb2bba62451494532db4fd41a97
kwk1024 <kwk1024@163.com>
2015-05-25 23:14:35 +0800

    add TestService

--- spring_mvc/src/main/java/com/kwk/HelloController.java
+++ spring_mvc/src/main/java/com/kwk/HelloController.java
@@ -5,12 +5,16 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
+
 @Controller
 public class HelloController {
+    @Resource
+    private TestService testService;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
-        model.addAttribute("message", "Hello Spring MVC Framework!");
+        model.addAttribute("message", testService.dump());
         return "hello";
     }
 
---
+++ spring_mvc/src/main/java/com/kwk/TestService.java
@@ -0,0 +1,7 @@
+package com.kwk;
+
+public class TestService {
+    public String dump() {
+        return "dump";
+    }
+}
